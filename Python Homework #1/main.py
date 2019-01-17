e1ffe21b17fccb3305fb15b143b64e30dcbc06f9
ad7608327 <46658259+ad7608327@users.noreply.github.com>
2019-01-17 06:12:42 +0200

    Add files via upload

---
+++ Python Homework #1/main.py
@@ -0,0 +1,13 @@
+# This Code is used to Describe My Favorite
+#Now, I Have Big Problem Because I dont Have Favorite
+#Music also Iam not keen on music but let say I like Jazz
+Name = "Skating in Central Park"
+Genre = "Jazz"
+Artist = "Jim Hall"
+DurationInSeconds = "324"
+platform= "Spotify"
+print(f"My Favourite Music is ({Name}\n it is ({Genre})) ")
+print(f"Actually Idont Care to know the Name of the Creator of Any Music\n anyway his Name is {Artist}\n I only Care About the Music\nTrust Me THIS MUSIC IS Very Good Nice Work {Artist}")
+print(f"In My Option it is Very Worried to Know LENGTH OF ANY Music Any Way it is {Duration Seconds} ")
+print(f"I think I Like this Kind of Music {Genre} Because it Help to be More Focus")
+print(f"I used {platform} to listen to {Genre} Music")
